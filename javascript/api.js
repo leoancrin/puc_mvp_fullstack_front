ac3d3da9907d1955afe67bd0427036c6292a5d25
Leonardo A. <leonardoancrin@gmail.com>
2026-09-26 01:19:10 -0300

    Adiciona rotas restantes da API

--- javascript/api.js
+++ javascript/api.js
@@ -24,10 +24,68 @@ export async function apiConsultarItens() {
     }
 }
 
+export async function apiConsultarItensTipoEspecifico(tipoItem) {
+    try {
+        const response = await fetch(`${CAMINHO_API}/consultar/itens/${tipoItem}`, {
+            method: "GET"
+        });
+
+        const dadosResposta = await response.json();
+
+        if (!response.ok) {
+            throw new Error(dadosResposta.Erro);
+        }
+        return dadosResposta;
+
+    } catch (error) {
+        throw error;
+    }
+}
+
 export async function apiCadastrarItens(dados) {
     try {
         const response = await fetch(`${CAMINHO_API}/cadastrar/itens`, {
             method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dados)
+        });
+
+        const dadosResposta = await response.json();
+
+        if (!response.ok) {
+            throw new Error(dadosResposta.Erro);
+        }
+        return dadosResposta;
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function apiDeletarItens(dados) {
+    try {
+        const response = await fetch(`${CAMINHO_API}/deletar/itens`, {
+            method: "DELETE",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dados)
+        });
+
+        const dadosResposta = await response.json();
+
+        if (!response.ok) {
+            throw new Error(dadosResposta.Erro);
+        }
+        return dadosResposta;
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function apiAlterarItens(dados) {
+    try {
+        const response = await fetch(`${CAMINHO_API}/alterar/itens`, {
+            method: "PUT",
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(dados)
         });
@@ -68,6 +126,48 @@ export async function apiCadastrarTipos(dados) {
     try {
         const response = await fetch(`${CAMINHO_API}/cadastrar/tipos`, {
             method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dados)
+        });
+
+        const dadosResposta = await response.json();
+
+        if (!response.ok) {
+            throw new Error(dadosResposta.Erro);
+        }
+
+        return dadosResposta;
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function apiDeletarTipos(dados) {
+    try {
+        const response = await fetch(`${CAMINHO_API}/deletar/tipos`, {
+            method: "DELETE",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dados)
+        });
+
+        const dadosResposta = await response.json();
+
+        if (!response.ok) {
+            throw new Error(dadosResposta.Erro);
+        }
+
+        return dadosResposta;
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function apiAlterarTipos(dados) {
+    try {
+        const response = await fetch(`${CAMINHO_API}/alterar/tipos`, {
+            method: "PUT",
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(dados)
         });
